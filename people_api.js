9834236bc3b9ed2070e7eb86b16bde186e69fe8d
James Terry <james.terry@mightyhive.com>
2020-08-13 13:13:47 +1000

    Adds sample pull from the people api
    https://developers.google.com/people/api/rest/v1/people/get
    resourceName: people/me
    personFields: externalIds,names,photos,emailAddresses

---
+++ people_api.js
@@ -0,0 +1,61 @@
+// resourceName: people/me
+// personFields: externalIds,names,photos,emailAddresses
+
+{
+  "resourceName": "people/102906295708167932663",
+  "etag": "%EgkBAj0DCT43Ji4aBAECBQciDGVoRUYrSjZ0YThBPQ==",
+  "names": [
+    {
+      "metadata": {
+        "primary": true,
+        "source": {
+          "type": "PROFILE",
+          "id": "102906295708167932663"
+        }
+      },
+      "displayName": "James Terry",
+      "familyName": "Terry",
+      "givenName": "James",
+      "displayNameLastFirst": "Terry, James",
+      "unstructuredName": "James Terry"
+    },
+    {
+      "metadata": {
+        "source": {
+          "type": "DOMAIN_PROFILE",
+          "id": "102906295708167932663"
+        }
+      },
+      "displayName": "James Terry",
+      "familyName": "Terry",
+      "givenName": "James",
+      "displayNameLastFirst": "James Terry",
+      "unstructuredName": "James Terry"
+    }
+  ],
+  "photos": [
+    {
+      "metadata": {
+        "primary": true,
+        "source": {
+          "type": "PROFILE",
+          "id": "102906295708167932663"
+        }
+      },
+      "url": "https://lh3.googleusercontent.com/a-/AOh14GgIZAM7xJi1Q-IepOijlPZnU25HAZEmL9Q_pqSAnw=s100"
+    }
+  ],
+  "emailAddresses": [
+    {
+      "metadata": {
+        "primary": true,
+        "verified": true,
+        "source": {
+          "type": "DOMAIN_PROFILE",
+          "id": "102906295708167932663"
+        }
+      },
+      "value": "james.terry@mightyhive.com"
+    }
+  ]
+}
